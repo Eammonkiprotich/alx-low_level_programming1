6974f22cb71b78c5deede5c53de58ee40ad55ae6
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-10 17:14:16 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * main - Prints with proper grammer, but the outcome is a piece of art
  * Return: always returns 0
  */
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
