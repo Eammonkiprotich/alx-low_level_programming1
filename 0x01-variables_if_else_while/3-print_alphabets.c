a425271bc56b16b3196e3f2ac57982cad39adfbd
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-12 13:22:29 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,16 +9,18 @@
 int main(void)
 {
         char array1[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-                'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+                'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		'y', 'z'};
 	char array2[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-                'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+                'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z'};
         int n;
         char nl;
 
         for (n = 0; n <= 25; n++)
                 putchar(array1[n]);
 	for (n =0; n <= 25; n++)
-	        putchar(array2[n]);	
+		putchar(array2[n]);
         nl = '\n';
         putchar(nl);
         return (0);
