976a76613e9bcc4ef66bf98a7e1cadb138e1bbd8
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-12 14:04:53 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.io>
+#include <stdio.h>
+
+/**
+ * main - This program prints number 1 to 10
+ * 
+ * Return: always return 0 when successful.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 11; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
