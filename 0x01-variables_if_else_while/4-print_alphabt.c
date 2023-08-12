4762b4185e9cdf88f5450100618b13a3b01e47ef
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-12 13:48:10 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints a to z in lowercase except q nad e
+ *
+ * Return: Always 0 if successful
+ */
+int main(void)
+{
+	char array[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	int n;
+	char nl;
+
+	for (n = 0; n <= 25; n++)
+	{
+		putchar(array[n]);
+		
+		if (array[n] == 'q' && array[n] == 'e')
+			break;
+	}
+	nl = '\n';
+	putchar(nl);
+	return (0);
+}
