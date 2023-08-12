8e0dd4c434744ad34f09f1807e372dcb8a15bd75
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-12 13:03:56 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,9 +11,11 @@ int main(void)
 	char array[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
 	     	'p','q','r','s','t','u','v','w','x','y','z'};
 	int n;
+	char nl;
 
 	for (n = 0; n<=25; n++)
 		putchar(array[n]);
-	putchar("\n");
+	nl = "\n";
+	putchar(nl);
 	return (0);
 }
