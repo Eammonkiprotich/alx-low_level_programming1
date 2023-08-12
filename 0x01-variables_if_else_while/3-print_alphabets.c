f3ac207b1913fafc9f0269123d7804f25c180b35
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-12 13:38:44 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-        char array1[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-		'j', 'k','l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+	char array1[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
 		'u', 'v', 'w', 'x',
 		'y', 'z'};
 	char array2[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
 		'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-		'U', 'V', 'W', 'X','Y', 'Z'};
+		'U', 'V', 'W', 'X', 'Y', 'Z'};
 	int n;
 	char nl;
 
