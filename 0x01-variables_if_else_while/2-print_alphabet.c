f30b9a345a8aaad811e9bb1f9c16f2874421ca93
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-12 12:54:11 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints a to z in lowercase
+ *
+ * Return: Always 0 if successful
+ */
+int main(void)
+{
+	char array[26] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+	     	'p','q','r','s','t','u','v','w','x','y','z'];
+	int n;
+
+	for (n = 0; n<=25; n++)
+		putchar(array[n]);
+	return (0);
+}
