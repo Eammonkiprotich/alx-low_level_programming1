6a353f5df0e61fea2bb5d455f743985f414130c9
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-12 22:53:03 -0700

    5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,16 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers from 0 to 9
+ * main - Prints numbers 1 to 9
  *
- * Return: 0
+ * Return: 0 if successful
+ *
  */
 int main(void)
 {
 	int n;
 
-	for ( n = 0; n < 10; n++)
-		printf("%d", n);
+	for (n = 0; n < 10; n++)
+		printf("%d), n);
 	printf("\n");
-	return (0);
+	return 0;
 }
