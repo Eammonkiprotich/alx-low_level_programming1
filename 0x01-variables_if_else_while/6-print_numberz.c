0e5896aab6d541da150ad352f3b8a6b9996c1c31
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-14 14:16:37 -0700

    correction

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <stdio/h>
+#include <stdio.h>
 
 /**
  * main - prints num 0 to 10 only using putchar without char variables
@@ -11,7 +11,7 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
-		putchar((num %10) + '0');
+		putchar((num % 10) + '0');
 	putchar('\n');
 	return (0);
 }
