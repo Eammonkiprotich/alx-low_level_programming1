42d900cdeb74cdf4ebcbb9197737d75bc2ec30eb
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-15 10:57:57 -0700

    correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,16 @@
  */
 int main(void)
 {
+	char nl;
+
 	_putchar('E');
 	_putchar('a');
+	_putchar('a');
+	_putchar('a');
+	_putchar('a');
+	_putchar('a');
+	_putchar('a');
+	nl = '\n';
+	_putchar('nl');
 	return (0);
 }
